d782cfaf91ce68a69ba662dc2dc62fd57fcd2f65
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-07-10 10:00:17 +0200

    EOD

--- main.rs
+++ main.rs
@@ -1,14 +1,34 @@
 use sysinfo::System;
+use std::io::Write;
+
+fn get_cpu_usages() -> Vec<f32> {
+    let mut v: Vec<f32> = Vec::new();
+    let mut sys = System::new();
+    sys.refresh_cpu(); // Refreshing CPU information.
+    for cpu in sys.cpus() {
+        v.push(cpu.cpu_usage());
+    }
+    return v;
+
+}
+
+fn get_average_cpu_usage() -> f32 {
+    let v = get_cpu_usages();
+    v.iter().sum::<f32>() as f32 / v.len() as f32
+}
 
 fn main() {
-    let mut sys = System::new();
+    //let mut sys = System::new();
     loop {
-        sys.refresh_cpu(); // Refreshing CPU information.
-        for cpu in sys.cpus() {
-        print!("{}% ", cpu.cpu_usage());
-        }
+        //sys.refresh_cpu(); // Refreshing CPU information.
+        //for cpu in sys.cpus() {
+        //    print!("{}% ", cpu.cpu_usage());
+        //}
+        println!("{}", get_average_cpu_usage());
+        let _ = std::io::stdout().flush();
         // Sleeping to let time for the system to run for long
         // enough to have useful information.
-        std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
+        let wait_time = std::time::Duration::from_millis(1000);
+        std::thread::sleep(wait_time);
     }
 }
